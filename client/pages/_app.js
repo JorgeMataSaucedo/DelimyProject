1d76cead3ad49ed47a5543ba4d6ca860d72d017f
JorgeMataSaucedo <miguel.matascd@uanl.edu.mx>
2023-03-27 15:14:05 -0600

    Primeros avances con Next js

---
+++ client/pages/_app.js
@@ -0,0 +1,9 @@
+import "../public/styles/global.css";
+import 'antd/dist/reset.css';
+
+
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
