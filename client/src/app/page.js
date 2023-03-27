a0e007951844e77d272a65124b63d74cebabef3e
JorgeMataSaucedo <miguel.matascd@uanl.edu.mx>
2023-03-27 16:11:04 -0600

    Primeros avances con Next js

---
+++ client/src/app/page.js
@@ -0,0 +1,15 @@
+
+import  "./globals.css"
+
+
+export default function Home() {
+  return (
+    <div className="container">
+        <main>
+            <h1 className="text-4xl bg-red-500">
+                Welcome to <a href="https://nextjs.org">Next.js!</a>
+            </h1>
+        </main>
+    </div>
+  )
+}
