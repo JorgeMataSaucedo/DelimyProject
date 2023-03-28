7f9bd4c60d2679e080e67192f4251c560afa6147
GabsFem <isabel.femerlingrmr@uanl.edu.mx>
2023-03-28 00:45:21 -0600

    Servicios aws
    mail aws y respuesta email

--- client/.next/static/webpack/webpack.607c10674f6a02c6.hot-update.js
+++ client/.next/static/webpack/webpack.607c10674f6a02c6.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "79efeb8aa7e94b95"; }
+/******/ 	__webpack_require__.h = function() { return "12c60055f5e10cf3"; }
 /******/ }();
 /******/ 
 /******/ }
